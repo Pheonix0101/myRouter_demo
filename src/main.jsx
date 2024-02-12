6c6cc67cc9cfcf2e2682c5bfeb23dce3d4720f29
Pheonix0101 <purushottam5252@gmail.com>
2024-02-12 13:25:39 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter,createRoutesFromElements,Route } from 'react-router-dom'
+import './index.css'
+import Layout from './Layout'
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import User from './components/user/User'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children:[
+//       {
+//         path: "",
+//         element: <Home/>
+//       },
+//       {
+//       path: "about",
+//       element: <About/>
+//       },
+//       {
+//         path:"contact",
+//         element:<Contact/>
+//       }
+//     ]
+
+//   }
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path="" element={<Home/>} />
+      <Route path="about" element={<About/>} />
+      <Route path="contact" element={<Contact/>} />
+      <Route path="user/:id" element={<User/>} />
+
+
+    </Route>
+
+    
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
